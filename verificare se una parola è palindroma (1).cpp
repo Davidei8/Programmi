ae5026257700395a1154cbe77dc74ac51c95664b
Davidei8 <89944748+Davidei8@users.noreply.github.com>
2022-11-29 08:45:56 +0100

    Update verificare se una parola è palindroma (1).cpp

--- verificare se una parola è palindroma (1).cpp
+++ verificare se una parola è palindroma (1).cpp
@@ -2,43 +2,32 @@
 #include <stdlib.h>
 using namespace std;
 
-int main()
-{
+int main(){
     int n, i, fl=0;
     char parola[100];
 
     cout<<"Inserire la lunghezza della parola da inserire:";
     cin>>n;
-    
     system("cls");
-    
-    for(i=0; i<=(n-1); i++)
-    {
+    for(i=0; i<=(n-1); i++){
         cout<<"Inserire la lettera:";
         cin>>parola[i];
     }
     
-    for(i=0; i<(n/2); i++)
-    {
-        if(parola[i]!=parola[n-1-i])
-        {
+    for(i=0; i<(n/2); i++){
+        if(parola[i]!=parola[n-1-i]){
             fl=1;
             i=n;
         }
-        else
-        {
+        else{
             fl=0;
         }
     }
-    
-    if(fl=0)
-    {
+    if(fl=0){
         cout<<"La parola e' palindroma \n";
     }
-    else
-    {
+    else{
         cout<<"La parola non e' palindroma \n";
     }
-    
     system("pause");
 }
